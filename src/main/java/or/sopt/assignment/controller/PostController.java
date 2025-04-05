d195e0948cbb6d2a5f1f9661288451429947c13d
gdbs1107 <gdbs1107@naver.com>
2025-04-05 17:25:53 +0900

    1차 과제

---
+++ src/main/java/or/sopt/assignment/controller/PostController.java
@@ -0,0 +1,35 @@
+package or.sopt.assignment.controller;
+
+import or.sopt.assignment.domain.Post;
+import or.sopt.assignment.service.PostService;
+
+import java.util.List;
+
+public class PostController {
+
+
+    private PostService postService = new PostService();
+    private int postId = 1;
+
+
+    public void createPost(String title){
+        Post newPost = new Post(postId++, title);
+
+        postService.createPost(newPost);
+    }
+
+
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    public Post getPostById(int id) {
+        return postService.getPostById(id);
+    }
+
+    public boolean deletePostById(int deleteId) {
+        postService.deletePostById(deleteId);
+
+        return true;
+    }
+}
---
+++ src/main/java/or/sopt/assignment/domain/Post.java
@@ -0,0 +1,21 @@
+package or.sopt.assignment.domain;
+
+
+public class Post {
+
+    private int id;
+    private String title;
+
+    public Post(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
