dbe3fd28b9d34c8ffc133440c83213efe8e10f9e
gdbs1107 <gdbs1107@naver.com>
2025-05-31 15:19:53 +0900

    fix: 게시글 관련 API 명세 수정

--- src/main/java/or/sopt/assignment/domain/comment/controller/CommentLikesController.java
+++ src/main/java/or/sopt/assignment/domain/comment/controller/CommentLikesController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/comments/likes")
-@Tag(name = "댓글 좋아요 관련 API")
+@Tag(name = "댓글 관련 API")
 public class CommentLikesController {
 
     private final CommentLikesService commentLikesService;
--- src/main/java/or/sopt/assignment/domain/post/controller/PostLikesController.java
+++ src/main/java/or/sopt/assignment/domain/post/controller/PostLikesController.java
@@ -9,26 +9,23 @@ import or.sopt.assignment.global.api.exception.status.SuccessStatus;
 import or.sopt.assignment.global.api.reponse.ApiResponseUtil;
 import or.sopt.assignment.global.api.reponse.ResponseDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/posts/likes")
 @RequiredArgsConstructor
-@Tag(name = "게시글 좋아요 관련 API")
+@Tag(name = "게시글 관련 API")
 public class PostLikesController {
 
     private final PostLikesService postLikesService;
 
-    @PostMapping("/{postId}/{userId}")
+    @PatchMapping("/{postId}/{userId}")
     @Operation(summary = "게시글 좋아요 API")
     public ResponseEntity<ResponseDTO<?>> like( @Parameter(description = "좋아요를 누를 게시글 ID") @PathVariable Long postId,
                                                 @Parameter(description = "좋아요를 누르는 사용자 ID") @PathVariable Long userId) {
 
         postLikesService.togglePostLike(postId,userId);
-        return ApiResponseUtil.ok(SuccessStatus._UPDATE_SUCCESS,"좋아요가 성공적으로 저장되었습니다");
+        return ApiResponseUtil.ok(SuccessStatus._UPDATE_SUCCESS);
     }
 
 }
